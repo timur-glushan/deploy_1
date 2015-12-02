a5597558c4a1169b959520dcd1d256157af224c7
Tymur Hlushan <timur@Tymurs-MacBook-Pro.local>
2015-12-02 12:05:25 +0200

    step_1 - local config

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+  <title>AWS Deployment Test #1 by IT-Giants</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+  <style type="text/css">
+@font-face {
+  font-family: 'Syncopate-Regular';
+  src:  url('media/Syncopate-Regular.ttf') format('ttf');
+}
+@font-face {
+  font-family: 'Syncopate-Bold';
+  src:  url('media/Syncopate-Bold.ttf') format('ttf');
+}
+  </style>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <style type="text/css">
+body { padding-top:60px; }
+.navbar-brand img { height:2em; margin:-0.5em 0 0 0; }
+article { min-height:10em; font-size:2em; }
+footer { font-size:2em; }
+  </style>
+  <script type="text/javascript">
+  </script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img src="images/it-giants-logo-text.png" alt="AWS Deployment Test #1 by IT-Giants">
+      </a>
+    </div>
+  </div>
+</nav>
+<header class="page-header">
+  <div class="container-fluid">
+    <h1>AWS Deployment Test #1 by IT-Giants <small></small></h1>
+  </div>
+</header>
+<article>
+  <div class="container-fluid">
+    <div class="col-sm-12">
+      <p><span class="label label-primary">Step 1 (current): Initial environment deployment.</span></p>
+    </div>
+  </div>
+</article>
+<footer>
+  <div class="container-fluid">
+    <div class="col-sm-12">
+      <p>Page generated on <b><?php echo date("Y-m-d"); ?></b> at <b><?php echo date("H:i:s"); ?></b></p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once("config.php");
+
+class TestSuite
+{
+  private $_testCases = [];
+
+  public function register($testCaseName, TestCase $testCaseInstance) {
+    $this->_testCases[$testCaseName] = $testCaseInstance;
+  }
+
+  public function execute() {
+    foreach ($this->_testCases as $testCaseName => $testCaseInstance) {
+      $result = NULL;
+      echo "\n[CASE:{$testCaseName}] Running... ";
+
+      try {
+        $result = $testCaseInstance->run();
+      } catch (Exception $e) {
+        $result = NULL;
+      }
+
+      if (!$result) {
+        echo "\nFAILED [CASE:{$testCaseName}] ";
+        exit(1);
+      }
+
+      echo "\nSUCCESS [CASE:{$testCaseName}] ";
+      var_export($result);
+    }
+
+   exit(0); 
+  }
+}
+
+
+
+abstract class TestCase
+{
+  private function up() {
+    // this code will do the processing before the test is executed. Normally that is for creating the test data in DB, etc.
+  }
+
+  private function down() {
+    // this code will do the processing after the test is executed. Normally that is for cleaning the test data in DB, etc.
+  }
+
+  abstract public function run();
+
+  public function get($url) {
+    $req = curl_init($url);
+    curl_setopt($req, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($req);
+    $info = curl_getinfo($req);
+    curl_close($req);
+    return $response;
+  }
+}
+
+
+
+class TestHomePage extends TestCase
+{
+  public function run() {
+    $page = $this->get(CLIENT_HOST_URL . CLIENT_HOST_PATH_HOME);
+    return strpos($page, "Step 1 (current): Initial environment deployment.") !== FALSE;
+  }
+}
+
+
+
+$testSuite = new TestSuite();
+$testHomePage = new TestHomePage();
+$testSuite->register("HomePage", $testHomePage);
+$testSuite->execute();
